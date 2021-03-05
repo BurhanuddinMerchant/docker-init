4243f1e06f4b876220203f1715ee977ce761a92a
Burhanuddin <bmerchant945@gmail.com>
2021-03-06 00:05:30 +0530

    docker init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send({ message: "Docer is easy as hell" });
+});
+app.listen(port, () => {
+  console.log("Server up on port : ", port);
+  console.log("Link : http://localhost:" + port);
+});
